4a93357b46bbb7bfbde8b1147a8028d505aa6602
Дмитрий Домрачев <dmitriy.domrachev@travelline.ru>
2019-03-18 11:31:13 +0300

    небольшие правки

--- app/js/basic-script.js
+++ app/js/basic-script.js
@@ -1,4 +1,4 @@
-var bookingButton = $('.booking-form__item_button');
+var bookingButton = $('.booking-form__item_submit');
 var roomtypeCard = $('.booking-form__item');
 
 bookingButton.on('click', function(){
@@ -6,6 +6,7 @@ bookingButton.on('click', function(){
     $(this).closest('.booking-form__item').on('mouseleave',function(){
         
         $(this).addClass('active-item')
+        $(this).find('.booking-form__item_submit').text('Подробнее');
         
     })
     
@@ -16,6 +17,7 @@ roomtypeCard.on('click', function(){
     
     if ($(this).hasClass('active-item')){
         $(this).removeClass('active-item');
+        $(this).find('.booking-form__item_submit').text('Забронировать');
         $(this).unbind('mouseleave');
     }  
     
