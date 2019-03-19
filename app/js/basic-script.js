a7087be15d722406680b2d3ec9183f76ab03fc48
Дмитрий Домрачев <dmitriy.domrachev@travelline.ru>
2019-03-19 09:24:43 +0300

    Скрипт с фильтрами, изменена верстка кнопок

--- app/js/basic-script.js
+++ app/js/basic-script.js
@@ -3,22 +3,26 @@ var roomtypeCard = $('.booking-form__item');
 
 bookingButton.on('click', function(){
    
-    $(this).closest('.booking-form__item').on('mouseleave',function(){
+   
+    $(this).closest('.booking-form__item').one('mouseleave',function(){
         
-        $(this).addClass('active-item')
+        $(this).gray();
+        $(this).addClass('active-item');
         $(this).find('.booking-form__item_submit').text('Подробнее');
         
     })
     
-    
+    event.preventDefault();
 })
 
 roomtypeCard.on('click', function(){
     
-    if ($(this).hasClass('active-item')){
+    
+        $('.grayscale').toggleClass('grayscale-off');;
         $(this).removeClass('active-item');
         $(this).find('.booking-form__item_submit').text('Забронировать');
-        $(this).unbind('mouseleave');
-    }  
+        
+      
     
 })
+
